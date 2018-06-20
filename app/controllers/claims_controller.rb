6de1b3e47533289bbc4ef089d5b3963bd283ebf5
Upshur06 <mupshur2005@gmail.com>
2018-06-19 22:00:34 -0400

    claims

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,14 +1,14 @@
 class ClaimsController < ApplicationController
   def index
-    if current_admin
-    @claims=Claim.all
-  else
-    redirect_to "/"
-  end
+      if current_admin
+      @claims=Claim.all
+      else
+      redirect_to "/"
+      end
   end
 
   def new
-
+    @claims=Claim.new
   end
 
   def create
@@ -16,22 +16,23 @@ class ClaimsController < ApplicationController
     claim.admin_id=current_admin.id
     claim.rent_id=params[:rent_id]
     if claim.save
-      redirect_to "/claims"
+    redirect_to "/claims"
     else
-      redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
     end
   end
 
   def show
+    @claims= Claim.find(params[:id])
   end
 
   def edit
+    @claims= Claim.find(params[:id])
   end
 
 private
 
   def claim_params
     params.require(:claim).permit(:admin_id, :rent_id, :description, :status, :type_of_claim, :price)
-
-end
+  end
 end
